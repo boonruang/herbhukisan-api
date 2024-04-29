90c03f297c3a561841cc53f3b70b7a2ad502f8bb
boonruang <scubatoy@gmail.com>
2024-04-29 22:39:35 +0700

    updated env

--- config/db-instance.js
+++ config/db-instance.js
@@ -4,9 +4,10 @@ const Sequelize = require('sequelize')
 const host = process.env.NODE_ENV_DB_HOST;
 const dbname = process.env.NODE_ENV_DB_NAME;
 const port = process.env.NODE_ENV_DB_PORT;
+const username = process.env.NODE_ENV_DB_USERNAME;
 const password = process.env.NODE_ENV_DB_PASSWORD;
 
-const sequelize = new Sequelize(dbname,'postgres',password,{
+const sequelize = new Sequelize(dbname,username,password,{
   host: host,
   port: port,
   dialect: 'postgres',
