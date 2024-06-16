d7c07172be1d7e2ed9941f84a0ce3e9071a57378
boonruang <scubatoy@gmail.com>
2024-06-16 16:02:56 +0700

    update farmer api

---
+++ app/api_farmer.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const router = express.Router()
+const formidable = require('formidable')
+const farmer = require('../models/farmer')
+const constants = require('../config/constant')
+const Sequelize = require('sequelize')
+const sequelize = require('../config/db-instance')
+const Op = Sequelize.Op
+
+//  @route                  GET  /api/v2/farmer/list
+//  @desc                   list all farmers
+//  @access                 Private
+router.get('/list', async (req, res) => {
+  console.log('get farmer API called')
+  try {
+    const farmerFound = await farmer.findAll({
+      order: [
+        ['id','ASC']
+      ],
+    })
+    if (farmerFound) {
+      console.log('farmerFound in list API: ', farmerFound)
+      res.status(200).json({
+        status: 'ok',
+        result: farmerFound,
+      })
+    } else {
+      res.status(500).json({
+        status: 'nok',
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      Error: error.toString(),
+    })
+  }
+})
+
+//  @route                  GET  /api/v2/farmer/select/:id
+//  @desc                   Get farmer by Id
+//  @access                 Private
+router.get('/select/:id', async (req, res) => {
+  console.log('get farmer by Id API called')
+  let id = req.params.id
+
+  try {
+    const farmerFound = await farmer.findOne({
+      where: { id },
+    })
+
+    if (farmerFound) {
+      res.status(200).json({
+        status: 'ok',
+        result: farmerFound,
+      })
+    } else {
+      res.status(500).json({
+        result: 'not found',
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+    })
+  }
+})
+
+
+//  @route                  POST  /api/v2/farmer
+//  @desc                   Post add farmer
+//  @access                 Private
+router.post('/', async (req, res) => {
+  console.log('farmer add is called')
+  
+  try {
+    const form = new formidable.IncomingForm();
+    console.log('form.parse(req)',form.parse(req))
+
+    form.parse(req, async (error, fields, files) => {
+      let result = await farmer.create(fields);
+      // result = await uploadImage(files, result);
+      console.log('req fields',fields)
+
+      res.json({
+        result: constants.kResultOk,
+        message: JSON.stringify(result)
+      });
+    });
+  } catch (error) {
+    res.json({
+      result: constants.kResultNok,
+      message: JSON.stringify(error)
+    });
+  }
+});
+
+module.exports = router
---
+++ models/farmer.js
@@ -0,0 +1,77 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../config/db-instance')
+
+const farmer = sequelize.define(
+  'farmers',
+  {
+    // attributes
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    firstname: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    cid: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    hno: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    moo: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },            
+    tambon: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    amphoe: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    province: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    tel: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },    
+    cert: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    cert_date: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    cert_expire_date: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "farmers",
+  },
+)
+
+;(async () => {
+  await farmer.sync({ force: false })
+})()
+
+module.exports = farmer
